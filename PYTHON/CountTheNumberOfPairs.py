f739bf24ea86b32ac173baf56fc10ed5bf337223
Adwait S Purao <104304028+Dare-marvel@users.noreply.github.com>
2023-03-30 10:04:42 +0530

    Update CountTheNumberOfPairs.py

--- PYTHON/CountTheNumberOfPairs.py
+++ PYTHON/CountTheNumberOfPairs.py
@@ -1,39 +1,41 @@
 # Link : https://codeforces.com/problemset/problem/1800/B
 
-# Key Insight : 
+# Key Insight : First we subtract the minimum frequency of the character and it's counterpart swapped case character(if it exists)
+# Then we check whether any charcter has frequency more than one so that the case of one of them can be swapped and we can get a pair
+# Also we need to check that we don't exceed the number of operation provided i.e. value of k , so we take the minimum of k and the 
+# calculated value ( we divide the frequency by 2 because 2 characters make one pair ) and we are interested in the number of pairs
 
 # Code :
 # read the number of test cases
 t = int(input())
-
 # loop over the test cases
 for _ in range(t):
     # read the values of n and k for this test case
     n, k = map(int, input().split())
     
-    # read the string for this test case
     newL = list(input().strip())  # strip() added to remove trailing whitespace
-    
+
     # initialize a dictionary to store the frequency of each character in the string
     burDict = {}
-    
+
+    # initialize a variable to store the number of pairs of characters that can be removed
+    burl = 0
+
     # loop over the characters in the string and update the dictionary
     for let in set(newL):
         burDict[let] = newL.count(let)
-    
-    # initialize a variable to store the number of pairs of characters that can be removed
-    burl = 0
-    
+
     # loop over the characters in the dictionary
     for char in burDict:
         # check if the uppercase/lowercase counterpart of the character is also in the dictionary
         if char.swapcase() in burDict:
-            # remove pairs of characters until one of the characters has no more remaining pairs
-            while burDict[char] > 0 and burDict[char.swapcase()] > 0:
-                burDict[char] -= 1
-                burDict[char.swapcase()] -= 1
-                burl += 1
-    
+            if burDict[char] > 0 and burDict[char.swapcase()] > 0:
+                # Directly adding the minimum number of pairs to the count of burls
+                pairs_to_add = min(burDict[char], burDict[char.swapcase()])
+                burl += pairs_to_add
+                burDict[char] -= pairs_to_add
+                burDict[char.swapcase()] -= pairs_to_add
+
     # loop over the characters in the dictionary again
     for i in burDict:
         # if the character has more than one occurrence and there are still pairs that can be removed
@@ -43,33 +45,5 @@ for _ in range(t):
             # update the total number of pairs and the remaining pairs
             burl += pairs_to_add
             k -= pairs_to_add
-    
     # print the total number of pairs that can be removed for this test case
-    print(burl)
-
-    
-    
-# Improved Code:
-t = int(input())
-for _ in range(t):
-    n, k = map(int, input().split())
-    newL = list(input().strip())  # strip() added to remove trailing whitespace
-    burDict = {}
-    burl = 0
-    for let in set(newL):
-        burDict[let] = newL.count(let)
-    for char in burDict:
-        if char.swapcase() in burDict:
-            if burDict[char] > 0 and burDict[char.swapcase()] > 0:
-                # Directly adding the minimum number of pairs to the count of burls
-                pairs_to_add = min(burDict[char],burDict[char.swapcase()])
-                burl += pairs_to_add
-                burDict[char] -= pairs_to_add
-                burDict[char.swapcase()] -= pairs_to_add
-                
-    for i in burDict:
-        if burDict[i] > 1 and k > 0:
-            pairs_to_add = min(burDict[i] // 2, k)
-            burl += pairs_to_add
-            k -= pairs_to_add
     print(burl)
